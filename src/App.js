b92226088dd8a1ea518297ae43427dc2e1f0ddca
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-04-13 15:26:36 +0200

    display data from JSON

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import songs from './data/songs.json';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Song Database</h1>
-		  <p>There are {songs.length} songs.</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Song Database</h1>
+			<p>There are {songs.length} songs.</p>
+			<ul>
+				{songs.map((song) => {
+					return (
+						<>
+							<li>{song.author} - {song.name}</li>
+						</>
+					)
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
