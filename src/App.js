cec2ae1b3f7dd0175d5fcd59f3ea402fe6dff828
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-04-13 15:17:03 +0200

    create site with title and test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import songs from './data/songs.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<h1>Song Database</h1>
+		  <p>There are {songs.length} songs.</p>
     </div>
   );
 }
